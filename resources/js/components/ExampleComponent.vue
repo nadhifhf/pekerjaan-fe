591c9eaf23cc2d6f0fb1bc99f6ae1ecff11ad276
nadhifhf <neorocket64@protonmail.com>
2021-11-05 07:56:23 +0700

    belum selesai

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -7,6 +7,10 @@
 
                     <div class="card-body">
                         I'm an example component.
+                        <v-client-table :columns="columns" :data="data" :options="options">
+                            <!-- <a slot="uri" slot-scope="props" target="_blank" :href="props.row.uri" class="glyphicon glyphicon-eye-open"></a> -->
+
+                        </v-client-table>
                     </div>
                 </div>
             </div>
@@ -16,8 +20,18 @@
 
 <script>
     export default {
+        data() {
+            return {
+                columns: ['code', 'qty', 'country_name', 'item_code', 'item_desc', 'product_type', 'grade', 'connection', 'size'],
+                data: []
+            }
+        },
         mounted() {
-            console.log('Component mounted.')
+            // console.log('Component mounted.');
+            axios.get('/api/data').then((response) => {
+                this.data = response.data;
+                console.log('test.');
+            });
         }
     }
 </script>
